6fdfeba28658af026cffdfca6bc4f61717afc4b4
Sergey Dziadevich <Sergey_Dziadevich@epam.com>
2018-11-09 19:49:38 +0300

    get started

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,8 @@
-$(function() {
-
-	// Custom JS
-
-});
+var url = 'https://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=eabd967104da4e07a9c41b1342a889b1';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
