ac55c45115df90b1df9d2c87895e7b7839ad94da
Sergey Dziadevich <Sergey_Dziadevich@epam.com>
2018-11-11 15:28:13 +0300

    select news channel

--- app/js/common.js
+++ app/js/common.js
@@ -35,19 +35,42 @@ function renderNews(elemId, news) {
     let elem = document.getElementById(elemId);
     if (elem && news.status == 'ok') {
         elem.innerHTML = news.articles
-            .map(a => '<li class="col-xs-12 col-lg-10 col-xl-8">' + render(a) + '</li>')
+            .map(a => '<li class="col-xs-12 col-lg-10 col-xl-8">' + renderNews(a) + '</li>')
             .reduce((txt, li) => txt + li, '<ul class="articles row">') + '</ul>';
     }
 }
 
-function render(article) {
+function renderNews(article) {
     return `<div class="card">
           <img class="card-img-top w-100" src="${article.urlToImage}" alt="${article.title}">
           <div class="card-block">
             <h4 class="card-title">${article.title}</h4>
             <p class="text-muted small">${(new Date(article.publishedAt)).toLocaleString()}</p>
             <p class="card-text">${article.description}</p>
-            <a href="${article.url}" target="_blank" class="btn btn-primary">Read on BBC</a>
+            <a href="${article.url}" target="_blank" class="btn btn-primary">Read more</a>
           </div>
         </div>`;
 }
+
+document.querySelector('.select-channel ul').addEventListener('click', function(e){
+
+    let menuList = document.querySelectorAll(".select-channel ul li");
+    menuList.forEach(function(item) {
+        item.classList.remove("active");
+    });
+
+    var newsSrc  = e.target.id;
+
+    e.target.classList.add("active");
+
+    urlNews = `https://newsapi.org/v2/top-headlines?sources=${newsSrc}&apiKey=${newsApiKey}`;
+
+    console.log(newsSrc);
+    console.log(urlNews);
+
+    fetch(new Request(urlNews))
+        .then(response =>  {
+            response.json().then(news => { renderNews(newsContainer, news) } );
+        });
+});
+
