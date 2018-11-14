f11d755aebee84b67ae6b6d8dc4b6033076ab852
Sergey Dziadevich <Sergey_Dziadevich@epam.com>
2018-11-14 12:08:12 +0300

    del comment

---
+++ app/js/RenderSources.js
@@ -0,0 +1,30 @@
+function renderSources(elemId, sources) {
+  let elem = elemId;
+  if (elem && sources.status == 'ok') {
+    elem.innerHTML = sources.sources
+      .map(s => `<li class="col-xs-6 col-sm-6 col-lg-3 col-xl-3" data-sourceid='${s.id}'>${s.name}</li>`)
+      .reduce((txt, li) => txt + li, '<ul class="row">') + '</ul>';
+
+    document.querySelector('#news-sources ul').addEventListener('click', function (e) {
+
+      let menuList = document.querySelectorAll("#news-sources ul li");
+      menuList.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      let newsSrc = e.target.getAttribute('data-sourceid');
+
+      e.target.classList.add("active");
+
+      urlNews = `https://newsapi.org/v2/top-headlines?sources=${newsSrc}&apiKey=${newsApiKey}`;
+
+      fetch(new Request(urlNews))
+        .then(response => {
+          response.json().then(news => {
+            renderNews(newsContainer, news)
+          });
+        });
+    });
+  }
+}
+
---
+++ app/js/SelectSources.js
@@ -0,0 +1,29 @@
+const newCategories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'],
+  sourcesContainer = document.querySelector('#news-sources');
+
+let htmlSelect = '<select class="form-control sel-cat" id="cat_val" name="cat-select">';
+
+htmlSelect += '<option value="0">Select news category:</option>';
+
+newCategories.forEach(value => {
+  htmlSelect += '<option value="' + value + '">' + value + '</option>';
+});
+
+htmlSelect += '</select>';
+
+document.getElementById("news-categories").innerHTML = htmlSelect;
+
+document.querySelector('.sel-cat').addEventListener('change', function (e) {
+  let selectedCategory = this.value;
+
+  let urlNews = `https://newsapi.org/v2/sources?category=${selectedCategory}&apiKey=${newsApiKey}`;
+
+  fetch(new Request(urlNews))
+    .then(response => {
+      response.json().then(sources => {
+        console.log(sources);
+        renderSources(sourcesContainer, sources);
+      });
+    });
+});
+
