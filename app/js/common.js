aa6bc5b27f2818c98054e5ef731e7b751fd224c4
Sergey Dziadevich <Sergey_Dziadevich@epam.com>
2018-11-14 12:03:10 +0300

    cleaning

--- app/js/common.js
+++ app/js/common.js
@@ -1,20 +1,6 @@
-//import articleRender from './articleCard.js';
-
 const newsContainer = document.getElementById('news-container'),
   newsApiKey = 'eabd967104da4e07a9c41b1342a889b1';
 
-
-// let newsSrc = 'bbc-news',
-//   urlNews = `https://newsapi.org/v2/top-headlines?sources=${newsSrc}&apiKey=${newsApiKey}`;
-//
-//
-// fetch(new Request(urlNews))
-//   .then(response => {
-//     response.json().then(news => {
-//       renderNews(newsContainer, news)
-//     });
-//   });
-//
 function renderNews(elemId, news) {
   let elem = elemId;
   if (elem && news.status == 'ok') {
@@ -23,26 +9,4 @@ function renderNews(elemId, news) {
       .reduce((txt, li) => txt + li, '<ul class="articles row">') + '</ul>';
   }
 }
-
-
-// document.querySelector('.select-channel ul').addEventListener('click', function (e) {
-//
-//   let menuList = document.querySelectorAll(".select-channel ul li");
-//   menuList.forEach(function (item) {
-//     item.classList.remove("active");
-//   });
-//
-//   let newsSrc = e.target.id;
-//
-//   e.target.classList.add("active");
-//
-//   urlNews = `https://newsapi.org/v2/top-headlines?sources=${newsSrc}&apiKey=${newsApiKey}`;
-//
-//   fetch(new Request(urlNews))
-//     .then(response => {
-//       response.json().then(news => {
-//         renderNews(newsContainer, news)
-//       });
-//     });
-// });
 
