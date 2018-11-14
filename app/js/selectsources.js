66586ba7e44c5ae2383f26f3f94e93a72f793e53
Sergey Dziadevich <developersiteweb@gmail.com>
2018-11-14 19:40:30 +0300

    interpolation

--- app/js/selectsources.js
+++ app/js/selectsources.js
@@ -2,12 +2,12 @@ const newCategories = ['business', 'entertainment', 'general', 'health', 'scienc
 const sourcesContainer = document.querySelector('#news-sources');
 
 let htmlSelect = '<select class="form-control sel-cat" id="cat_val" name="cat-select">';
-
 htmlSelect += '<option value="0">Select news category:</option>';
 newCategories.forEach(value => {
-  htmlSelect += '<option value="' + value + '">' + value + '</option>';
+  htmlSelect += `<option value="${value}">${value}</option>`;
 });
 htmlSelect += '</select>';
+
 
 document.getElementById("news-categories").innerHTML = htmlSelect;
 
