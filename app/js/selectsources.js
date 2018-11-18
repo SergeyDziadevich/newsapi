adec725645a8d6576502f7ddd19f2ff6202f2835
Sergey Dziadevich <developersiteweb@gmail.com>
2018-11-18 14:54:53 +0300

    a shorter entry using the interpolation + reduce

--- app/js/selectsources.js
+++ app/js/selectsources.js
@@ -1,12 +1,10 @@
 const newCategories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
 const sourcesContainer = document.querySelector('#news-sources');
 
-let htmlSelect = '<select class="form-control sel-cat" id="cat_val" name="cat-select">';
-htmlSelect += '<option value="0">Select news category:</option>';
-newCategories.forEach(value => {
-  htmlSelect += `<option value="${value}">${value}</option>`;
-});
-htmlSelect += '</select>';
+let htmlSelect = `
+  <select class="form-control sel-cat" id="cat_val" name="cat-select">
+    ${newCategories.reduce((allOpt, opt) => allOpt + `<option value="${opt}">${opt}</option>`, `<option value="0">Select news category:</option>`)}
+  </select>`;
 
 document.getElementById('news-categories').innerHTML = htmlSelect;
 
