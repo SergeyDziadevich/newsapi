8fa8008ff3ecad9035548839831a58c83cbcaabc
Sergey Dziadevich <developersiteweb@gmail.com>
2018-11-18 15:20:12 +0300

    the right style initialization of variables and a shorter entry using the interpolation + reduce

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,10 @@
-const newsContainer = document.getElementById('news-container'),
-  newsApiKey = 'eabd967104da4e07a9c41b1342a889b1';
+const newsContainer = document.getElementById('news-container');
+const newsApiKey = 'eabd967104da4e07a9c41b1342a889b1';
 
 function renderNews(elemId, news) {
   let elem = elemId;
   if (elem && news.status == 'ok') {
-    elem.innerHTML = news.articles
-      .map(a => '<li class="col-xs-12 col-sm-12 col-lg-6 col-xl-6">' + render(a) + '</li>')
-      .reduce((txt, li) => txt + li, '<ul class="articles row">') + '</ul>';
+    elem.innerHTML = news.articles.reduce((txt, li) => txt + `<li class='col-xs-12 col-sm-12 col-lg-6 col-xl-6'>${render(li)}</li>`, `<ul class="articles row">`) + `</ul>`;
   }
 }
 
