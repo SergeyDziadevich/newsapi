5102b712abd0aae4e590c8d3baa07e6c2dec6904
Sergey Dziadevich <Sergey_Dziadevich@epam.com>
2018-11-21 14:05:16 +0300

    fixed a long line

--- app/js/common.js
+++ app/js/common.js
@@ -4,12 +4,13 @@ import {renderSources} from './sources';
 const newCategories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
 const sourcesContainer = document.querySelector('#news-sources');
 
-let htmlSelect = `
+let htmlSelectCategories = `
   <select class="form-control sel-cat" id="cat_val" name="cat-select">
-    ${newCategories.reduce((allOpt, opt) => allOpt + `<option value="${opt}">${opt}</option>`, `<option value="0">Select news category:</option>`)}
+    ${newCategories.reduce((allOpt, opt) => allOpt + `<option value="${opt}">${opt}</option>`,
+  `<option value="0">Select news category:</option>`)}
   </select>`;
 
-document.getElementById('news-categories').innerHTML = htmlSelect;
+document.getElementById('news-categories').innerHTML = htmlSelectCategories;
 
 document.querySelector('.sel-cat').addEventListener('change', e => {
   let selectedCategory = e.target.value;
--- app/js/sources.js
+++ app/js/sources.js
@@ -7,7 +7,8 @@ function renderSources(elemId, sources) {
   let elem = elemId;
   if (elem && sources.status == 'ok') {
     elem.innerHTML = sources.sources
-      .reduce((txt, li) => txt + `<li class="col-xs-6 col-sm-6 col-lg-3 col-xl-3" data-sourceid='${li.id}'>${li.name}</li>`, `<ul class='row'>`) + '</ul>';
+      .reduce((txt, li) => txt + `<li class="col-xs-6 col-sm-6 col-lg-3 col-xl-3" data-sourceid='${li.id}'>
+        ${li.name}</li>`, `<ul class='row'>`) + '</ul>';
 
     document.querySelector('#news-sources ul').addEventListener('click', e => {
 
