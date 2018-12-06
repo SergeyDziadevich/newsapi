cf39a0bb816ad6d1012dc0d7e1d3909fbacb9b4a
Sergey Dziadevich <developersiteweb@gmail.com>
2018-12-06 16:57:04 +0300

    components button

---
+++ app/js/components/button/button.js
@@ -0,0 +1,17 @@
+import './btn-shownews.scss';
+
+const body = document.querySelector('body');
+
+const button = document.createElement('button');
+
+button.textContent = 'Show News';
+button.className = 'btn btn-danger btn-shownews';
+button.addEventListener('click', loadNews);
+body.appendChild(button);
+
+async function loadNews() {
+  await import('../../common');
+
+  const parent = button.parentElement;
+  parent.removeChild(button);
+}
--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,7 @@
 import 'bootstrap/scss/bootstrap.scss';
-import '../scss/btn-shownews.scss';
+
+import './components/button/button';
 
 //import './test.json';
 
-const body = document.querySelector('body');
-
-const button = document.createElement('button');
-
-button.textContent = 'Show News';
-button.className = 'btn btn-danger btn-shownews';
-button.addEventListener('click', loadNews);
-body.appendChild(button);
-
-async function loadNews() {
-  await import('./common');
-
-  const parent = button.parentElement;
-  parent.removeChild(button);
-}
 
