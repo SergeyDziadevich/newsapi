68d07303c1318a12ba23da7d86ab115d8e1a3766
Sergey Dziadevich <developersiteweb@gmail.com>
2018-12-06 18:28:36 +0300

    refacoring

--- app/js/common.js
+++ app/js/common.js
@@ -7,7 +7,6 @@ import {renderSources} from './sources';
 const newCategories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
 const sourcesContainer = document.querySelector('#news-sources');
 
-
 let htmlSelectCategories = `
   <select class="form-control sel-cat" id="cat_val" name="cat-select">
     ${newCategories.reduce((allOpt, opt) => allOpt + `<option value="${opt}">${opt}</option>`,
@@ -20,8 +19,6 @@ document.querySelector('.sel-cat').addEventListener('change', e => {
   let selectedCategory = e.target.value;
 
   async function getSource(){
-    // let requestApi = FactoryApi.create('getsources', selectedCategory);
-    // let sources = await requestApi.getApi();
 
     let requestApi = LogApi.create('getsources', selectedCategory);
     let sources = await requestApi.getApi();
