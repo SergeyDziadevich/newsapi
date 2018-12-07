1e3529985e8902c2332133ba699a73c0ace93279
Sergey Dziadevich <developersiteweb@gmail.com>
2018-12-07 08:55:30 +0300

    separation of logic and view

--- app/js/common.js
+++ app/js/common.js
@@ -1,19 +1,17 @@
 import '../scss/main.scss';
 
+import SelectCategories from './components/select-categories/select-categories';
+import News from './components/news/news';
+import Sources from './components/sources/sources';
 import {LogApi} from './proxy';
 
-import {renderSources} from './sources';
-
-const newCategories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
 const sourcesContainer = document.querySelector('#news-sources');
+const newsContainer = document.getElementById('news-container');
+const compCategories = new SelectCategories();
+const compSources = new Sources();
+const compNews = new News();
 
-let htmlSelectCategories = `
-  <select class="form-control sel-cat" id="cat_val" name="cat-select">
-    ${newCategories.reduce((allOpt, opt) => allOpt + `<option value="${opt}">${opt}</option>`,
-  `<option value="0">Select news category:</option>`)}
-  </select>`;
-
-document.getElementById('news-categories').innerHTML = htmlSelectCategories;
+compCategories.create();
 
 document.querySelector('.sel-cat').addEventListener('change', e => {
   let selectedCategory = e.target.value;
@@ -23,7 +21,48 @@ document.querySelector('.sel-cat').addEventListener('change', e => {
     let requestApi = LogApi.create('getsources', selectedCategory);
     let sources = await requestApi.getApi();
 
-    renderSources(sourcesContainer, sources);
+    compSources.renderSources(sourcesContainer, sources);
+
+    sourcesListener();
   }
   getSource();
 });
+
+function sourcesListener() {
+  document.querySelector('#news-sources ul').addEventListener('click', e => {
+
+    let menuList = document.querySelectorAll("#news-sources ul li");
+    menuList.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    let newsSrc = e.target.getAttribute('data-sourceid');
+
+    e.target.classList.add('active');
+
+    async function getNews(){
+      try {
+        let requestApi = LogApi.create('getnews', newsSrc);
+        let news = await requestApi.getApi();
+
+
+        //simulate error if news in source > 10
+        if(news.totalResults >= 11){
+          throw new Error('too many results');
+        }
+
+        compNews.renderNews(newsContainer, news);
+
+      } catch (err) {
+
+        console.log(err.message);
+
+        //Lazy Initialization
+        import( './components/modal/modal').then(
+          ({ default: errorElement }) => { document.body.appendChild(errorElement); }
+        );
+      }
+    }
+    getNews();
+  });
+}
