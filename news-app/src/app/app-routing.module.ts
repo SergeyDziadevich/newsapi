7bcdb1e56ddebb6ec8693984e80a50e76e1057d2
Sergey Dziadevich <Sergey_Dziadevich@epam.com>
2019-02-14 17:24:11 +0300

    Add routing

--- news-app/src/app/app-routing.module.ts
+++ news-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewsListComponent } from "./news-list/news-list.component";
+import { ArticlePageComponent } from "./article-page/article-page.component";
+import { ArticleFormComponent } from "./article-form/article-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NewsListComponent},
+  {path: 'article/:id', component: ArticlePageComponent},
+  {path: 'article-edit/:id', component: ArticleFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- news-app/src/app/app.module.ts
+++ news-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsListComponent } from './news-list/news-list.component';
+import { ArticlePageComponent } from './article-page/article-page.component';
+import { ArticleFormComponent } from './article-form/article-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubHeaderComponent,
+    FooterComponent,
+    NewsListComponent,
+    ArticlePageComponent,
+    ArticleFormComponent
   ],
   imports: [
     BrowserModule,
